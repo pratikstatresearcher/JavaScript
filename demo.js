a129b52defb54a67be87696243b8479d5e32e4b2
Pratik Ugral <pratik.statresearcher@gmail.com>
2024-12-10 02:28:52 +0000

    demo file

---
+++ demo.js
@@ -0,0 +1,7 @@
+console.log("I am Pratik");
+//************************Its a demo file********************//
+
+// We can define variable by using "let" / "var" keyword //
+let age = 45;
+const user_name = "Pratik";
+console.table([user_name, age]);
